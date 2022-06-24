350234fcc865b42884d8058eb67c4e55792a6bf1
AlinIonut7 <107341931+AlinIonut7@users.noreply.github.com>
2022-06-24 13:26:19 +0300

    Add files via upload

---
+++ Proiect1/src/main/java/com/cv/Prezentare.java
@@ -0,0 +1,31 @@
+package com.cv;
+
+public class Prezentare {
+	private long id = 0;
+	private String content = "";
+
+	 
+	
+
+	
+	
+
+	public Prezentare(Integer integer, String nume) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void Greeting(long id, String content) {
+		this.id = id;
+		this.content = content;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+	
+	}
+
---
+++ Proiect1/src/main/java/com/cv/TrackController.java
@@ -0,0 +1,11 @@
+package com.cv;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TrackController {
+	@GetMapping("/prezentare");
+	
+	
+
+}
